7b10c01e115e31a6185da6914df895a40f4d60f2
Dolby Developer <jeang@jeang-mba.dolby.net>
2017-03-15 09:41:54 -0700

    Dolby Atmos Update
    Updating to include Dolby Atmos Support

--- Dolby.UAP/Dolby.UAP/Properties/AssemblyInfo.cs
+++ Dolby.UAP/Dolby.UAP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dolby.UAP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Dolby Atmos® media player demo")]
+[assembly: AssemblyDescription("A Universal Windows Platform application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dolby.UAP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Dolby Laboratories, Inc.")]
+[assembly: AssemblyProduct("Dolby Atmos® media player demo")]
+[assembly: AssemblyCopyright("Copyright © 2017 Dolby Laboratories, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: ComVisible(false)]
